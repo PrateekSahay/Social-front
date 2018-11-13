efb1aa8313b86226a45109edef4dbec7c1131204
aman5prakash <amanprakash512@gmail.com>
2018-11-13 12:48:36 +0530

    home style

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,28 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  level: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', level: 3},
+  {position: 2, name: 'Helium', level: 2},
+  {position: 3, name: 'Lithium', level: 7},
+  {position: 4, name: 'Beryllium', level: 9},
+  {position: 5, name: 'Boron', level: 6},
+];
+
+
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-
+  displayedColumns: string[] = ['position', 'name', 'level'];
+  dataSource = ELEMENT_DATA;
   constructor() { }
 
   ngOnInit() {
